00b6caa478498bfd4ceaf27c839c57cce410d29e
SHATHA SULTAN AL-SWAILAM <154978695+shathasultan@users.noreply.github.com>
2026-01-03 21:33:40 +0300

    Remove comment in user.py constructor

--- Part2/models/user.py
+++ Part2/models/user.py
@@ -2,7 +2,7 @@ import uuid
 
 class User:
     def __init__(self, first_name, last_name, email):
-        self.id = str(uuid.uuid4())  # إنشاء ID فريد تلقائياً
+        self.id = str(uuid.uuid4()) 
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
