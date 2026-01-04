8c892113dfe3a9af13bcffbb136b707f74546b43
NSF9 <alzhraniii999@gmail.com>
2026-01-05 00:41:50 +0300

    update

--- Part2/hbnb/app/__init__.py
+++ Part2/hbnb/app/__init__.py
@@ -3,6 +3,6 @@ from flask_restx import Api
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app, title="HBnB API", version="1.0")
+    api = Api(app, title = "Hbnb API", description = "Hbnb Project API", version="1.0")
 
     return app
