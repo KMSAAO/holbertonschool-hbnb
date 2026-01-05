653244510fe697a0bd1d082ddbf3e69ab4c33f98
Nawaf Saleh <128853300+NSF9@users.noreply.github.com>
2026-01-05 06:43:55 +0300

    Update facade.py

---
+++ Part2/hbnb/app/services/facade.py
@@ -0,0 +1,10 @@
+from app.models.amentiy import Amenity
+   #implement these methods later
+    def create_amenity(self):
+        pass
+    def get_amenity_info(self): 
+        pass
+    def update_amenity(self):
+        pass
+    def delete_amenity(self):
+        pass
