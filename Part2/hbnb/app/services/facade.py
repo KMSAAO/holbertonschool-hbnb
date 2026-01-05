bf850d1b78018acbfa7cf31720828245e3dbc8f1
Nawaf Saleh <128853300+NSF9@users.noreply.github.com>
2026-01-05 07:02:06 +0300

    Update facade.py

--- Part2/hbnb/app/services/facade.py
+++ Part2/hbnb/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.models.amentiy import Amenity
+from app.models.place import Place
    #implement these methods later
     def create_amenity(self):
         pass
@@ -7,4 +8,15 @@ from app.models.amentiy import Amenity
     def update_amenity(self):
         pass
     def delete_amenity(self):
+        pass
+      
+      
+        #implement these methods later
+    def create_place(self):
+        pass
+    def get_place_info(self): 
+        pass
+    def update_place(self):
+        pass
+    def delete_place(self):
         pass
