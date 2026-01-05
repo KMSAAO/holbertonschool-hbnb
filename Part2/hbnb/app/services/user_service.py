aa6306c8a425f512fadb50731496dd60199283fe
Nawaf Saleh <128853300+NSF9@users.noreply.github.com>
2026-01-06 02:45:55 +0300

    Update user_service.py

---
+++ Part2/hbnb/app/services/user_service.py
@@ -0,0 +1,13 @@
+from app.models.user import User
+
+class UserServices:
+
+    #implement these methods later
+    def login(self):
+        pass
+    def register_users(self):
+        pass
+    def get_user_info(self):
+        pass
+    def update_users(self):
+        pass
