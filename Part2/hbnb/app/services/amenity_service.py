27d0d9b6765e1c6ea84c21865618e1c16bec231a
Khalid alomari <khalidalomari199@gmail.com>
2026-01-06 05:00:04 +0300

    add the methods for amenity_service

---
+++ Part2/hbnb/app/services/amenity_service.py
@@ -0,0 +1,45 @@
+from app.models.amenity import Amenity
+
+class Amenity_Service():
+    def create_amenity(self, amenity_data, repo):
+        name = amenity_data.get('name')
+        desc = amenity_data.get('description')
+        if not name:
+            raise ValueError("Amenity name is required")
+        
+        if len(desc) > 250:
+            raise ValueError("Description is too long")
+        amenity = Amenity(**amenity_data)
+
+        repo.add(amenity)
+
+        return amenity
+
+    def get_amenity_info(self, amenity_id, repo): 
+        amenity = repo.get(amenity_id)
+
+        if not amenity:
+            return None
+        return amenity
+
+    def update_amenity(self, amenity_id, update_data, repo):
+        amenity = repo.get(amenity_id)
+        if not amenity:
+            return None
+        
+        forbidden_keys = ['id', 'created_at', 'updated_at']
+
+        for key, value in update_data.items():
+            if key in forbidden_keys:
+                continue
+            
+            if hasattr(amenity, key):
+                setattr(amenity, key, value)
+
+        amenity.last_updated()
+
+        return amenity
+    def delete_amenity(self, amenity_id, repo):
+        success = repo.delete(amenity_id)
+
+        return success
