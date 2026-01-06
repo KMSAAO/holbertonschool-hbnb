4a835392ff708fb837ed520d14d82fd4643ee6b8
SHATHA SULTAN AL-SWAILAM <154978695+shathasultan@users.noreply.github.com>
2026-01-06 18:31:42 +0300

    Implement amenities API with CRUD operations

---
+++ Part2/hbnb/app/api/v1/amenities.py
@@ -0,0 +1,21 @@
+from flask_restx import Namespace, Resource, fields
+from app.services import facade
+
+api = Namespace('amenities', description='Amenity operations')
+
+amenity_model = api.model('Amenity', {
+    'name': fields.String(required=True, description='Name of the amenity')
+})
+
+@api.route('/')
+class AmenityList(Resource):
+    @api.expect(amenity_model, validate=True)
+    @api.response(201, 'Amenity successfully created')
+    def post(self):
+        amenity_data = api.payload
+        new_amenity = facade.create_amenity(amenity_data)
+        return {'id': new_amenity.id, 'name': new_amenity.name}, 201
+
+    def get(self):
+        amenities = facade.get_all_amenities()
+        return [{'id': a.id, 'name': a.name} for a in amenities], 200
