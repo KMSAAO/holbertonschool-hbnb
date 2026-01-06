5c261b203b27c553b75f97887b3a2436bef8abe8
SHATHA SULTAN AL-SWAILAM <154978695+shathasultan@users.noreply.github.com>
2026-01-06 23:41:57 +0300

    Update facade.py

--- Part2/hbnb/app/services/facade.py
+++ Part2/hbnb/app/services/facade.py
@@ -2,15 +2,19 @@ from app.persistence.repository import InMemoryRepository
 from app.services.user_service import User_Service
 from app.services.place_service import Place_Service
 from app.services.amenity_service import Amenity_Service
+from app.services.review_service import Review_Service
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
         self.place_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+
         self.user_service = User_Service()
         self.place_service = Place_Service()
-        self.amenity_repo = InMemoryRepository()
         self.amenity_service = Amenity_Service()
+        self.review_service = Review_Service()
 
     # --- User Methods ---
     def create_user(self, user_data):
@@ -24,7 +28,6 @@ class HBnBFacade:
 
     # --- Place Methods ---
     def create_place(self, place_data):
-        # We pass both repos because place creation needs to verify the user exists
         return self.place_service.create_place(place_data, self.place_repo, self.user_repo)
 
     def get_place(self, place_id):
@@ -36,22 +39,24 @@ class HBnBFacade:
     def update_place(self, place_id, place_data):
         return self.place_service.update_place(place_id, place_data, self.place_repo)
 
+    # --- Amenity Methods ---
+    def create_amenity(self, amenity_data):
+        return self.amenity_service.create_amenity(amenity_data, self.amenity_repo)
 
-    def create_amenity(self, amenity_1):
-        return self.amenity_service.create_amenity(amenity_1, self.amenity_repo)
-    def get_amenity_info(self): 
-        pass
-    def update_amenity(self):
-        pass
-    def delete_amenity(self):
-        pass
-    def create_Review(self):
-        pass
-    def get_Review_info(self): 
-        pass
-    def update_Review(self):
-        pass
-    def delete_Review(self):
-        pass
+    def get_amenity(self, amenity_id):
+        return self.amenity_service.get_amenity_info(amenity_id, self.amenity_repo)
+
+    def get_all_amenities(self):
+        return self.amenity_service.get_all_amenities(self.amenity_repo)
+
+    # --- Review Methods ---
+    def create_review(self, review_data):
+        return self.review_service.create_review(review_data, self.review_repo, self.user_repo, self.place_repo)
+
+    def get_review(self, review_id):
+        return self.review_service.get_review_info(review_id, self.review_repo)
+
     def get_all_reviews(self):
-        pass
+        return self.review_service.get_all_reviews(self.review_repo)
+
+facade = HBnBFacade()
