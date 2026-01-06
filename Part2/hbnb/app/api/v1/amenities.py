9603d25e4468ca90810551c66eebdbe34fe42d5b
SHATHA SULTAN AL-SWAILAM <154978695+shathasultan@users.noreply.github.com>
2026-01-06 23:55:44 +0300

    Update amenities.py

--- Part2/hbnb/app/api/v1/amenities.py
+++ Part2/hbnb/app/api/v1/amenities.py
@@ -1,13 +1,21 @@
-from app.models.amenity import Amenity
+from flask_restx import Namespace, Resource, fields
+from app.services import facade
 
-class Amenity_Service:
-    def create_amenity(self, amenity_data, repo):
-        new_amenity = Amenity(**amenity_data)
-        repo.add(new_amenity)
-        return new_amenity
+api = Namespace('amenities', description='Amenity operations')
 
-    def get_amenity_info(self, amenity_id, repo):
-        return repo.get(amenity_id)
+amenity_model = api.model('Amenity', {
+    'name': fields.String(required=True, description='Name of the amenity')
+})
 
-    def get_all_amenities(self, repo):
-        return repo.get_all()
+@api.route('/')
+class AmenityList(Resource):
+    def get(self):
+        """List all amenities"""
+        amenities = facade.get_all_amenities()
+        return [{'id': a.id, 'name': a.name} for a in amenities], 200
+
+    @api.expect(amenity_model, validate=True)
+    def post(self):
+        """Create a new amenity"""
+        new_amenity = facade.create_amenity(api.payload)
+        return {'id': new_amenity.id, 'name': new_amenity.name}, 201
