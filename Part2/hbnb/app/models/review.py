9567cd0043af27d3018d5d7b685da0673eeb3259
Nawaf <Alzhraniii999@gmail.com>
2026-01-08 17:38:12 +0300

    fix bugs

--- Part2/hbnb/app/models/review.py
+++ Part2/hbnb/app/models/review.py
@@ -1,12 +1,13 @@
 from app.models.base_model import BaseModel
+from app.models.place import Place
+from app.models.user import User
 
 class Review(BaseModel):
 
-    def __init__(self, place_id, user_id, rating, comment):
+    def __init__(self, place: Place, user: User, rating, comment):
 
         super().__init__()
-
-        self.place_id = place_id
-        self.user_id = user_id
+        self.place = place
+        self.user = user
         self.rating = rating
         self.comment = comment
