1d7522cc70afb3b236961d12a18e31f7b2440700
NSF9 <alzhraniii999@gmail.com>
2026-01-19 12:32:56 +0300

    new update

---
+++ Part2/app/api/v1/places.py
@@ -0,0 +1,112 @@
+from flask_restx import Namespace, Resource, fields
+from app.services import facade
+
+api = Namespace('places', description='Place operations')
+
+place_create_model = api.model('PlaceCreate', {
+    'owner_id':    fields.String(required=True, description='ID of the owner (User)'),
+    'title':       fields.String(required=True, description='Title of the place'),
+    'description': fields.String(required=False, description='Description of the place'),
+    'price':       fields.Float(required=True, description='Price of the place'),
+    'status':      fields.String(required=False, description='Status of the place'),
+    'latitude':    fields.Float(required=True, description='Latitude'),
+    'longitude':   fields.Float(required=True, description='Longitude'),
+})
+
+place_response_model = api.model('PlaceResponse', {
+    'id':          fields.String,
+    'owner_id':    fields.String,
+    'title':       fields.String,
+    'description': fields.String,
+    'price':       fields.Float,
+    'status':      fields.String,
+    'latitude':    fields.Float,
+    'longitude':   fields.Float,
+    'created_at':  fields.String,
+    'updated_at':  fields.String,
+})
+
+place_update_model = api.model('PlaceUpdate', {
+    'title':       fields.String(required=False),
+    'description': fields.String(required=False),
+    'price':       fields.Float(required=False),
+    'status':      fields.String(required=False),
+    'latitude':    fields.Float(required=False),
+    'longitude':   fields.Float(required=False),
+    'owner_id':    fields.String(required=False)
+})
+
+@api.route('/')
+class PlaceList(Resource):
+
+    @api.expect(place_create_model, validate=True)
+    @api.marshal_with(place_response_model, code=201)
+    @api.response(400, 'Validation / business error')
+    def post(self):
+        """Create a new place"""
+        data = api.payload
+
+        try:
+            place = facade.create_place(place_data=data)
+        except ValueError as e:
+            api.abort(400, str(e))
+
+        return place, 201
+
+
+@api.route('/<string:place_id>')
+@api.response(404, 'Place not found')
+class PlaceDetail(Resource):
+
+    @api.marshal_with(place_response_model, code=200)
+    def get(self, place_id):
+        """Get place info by ID"""
+        try:
+            place = facade.get_place_info(place_id=place_id)
+        except ValueError as e:
+            api.abort(404, str(e))
+
+        return place, 200
+
+    @api.expect(place_update_model, validate=False)
+    @api.marshal_with(place_response_model, code=200)
+    @api.response(400, 'Validation / business error')
+    def put(self, place_id):
+        """Update place by ID"""
+        data = api.payload or {}
+
+        try:
+            updated = facade.update_place(
+                place_id=place_id,
+                place_data=data
+            )
+        except ValueError as e:
+            msg = str(e)
+            if "not found" in msg.lower():
+                api.abort(404, msg)
+            api.abort(400, msg)
+        if not updated:
+            api.abort(404, "Place not found")
+
+        try:
+            place = facade.get_place_info(place_id=place_id)
+            return place, 200
+        except ValueError as e:
+            api.abort(404, str(e))
+
+    @api.response(204, 'Place deleted')
+    @api.response(400, 'Delete error')
+    def delete(self, place_id):
+        """Delete place by ID"""
+        try:
+            deleted = facade.delete_place(place_id=place_id)
+        except ValueError as e:
+            msg = str(e)
+            if "not found" in msg.lower():
+                api.abort(404, msg)
+            api.abort(400, msg)
+
+        if not deleted:
+            api.abort(404, "Place not found")
+
+        return '', 204
---
+++ Part2/app/services/user_service.py
@@ -0,0 +1,165 @@
+from app.models.user import User
+import re, hashlib, datetime
+
+class UserServices():
+
+
+    def register_users(self, user_data: dict, repo):
+
+        existing_user = repo.get_by_attribute('email', user_data.get('email'))
+        if existing_user:
+            raise ValueError("Email already registered")
+
+       
+        first_name = user_data.get('first_name')
+        if not first_name or not isinstance(first_name, str) or len(first_name) > 50:
+            raise ValueError("First name is required and must be a string with max 50 characters")
+
+       
+        last_name = user_data.get('last_name')
+        if not last_name or not isinstance(last_name, str) or len(last_name) > 50:
+            raise ValueError("Last name is required and must be a string with max 50 characters")
+
+        
+        email = user_data.get('email')
+        if not email or not isinstance(email, str):
+            raise ValueError("Email is required and must be a string")
+        email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        if not re.match(email_pattern, email):
+            raise ValueError("Invalid email format")
+
+        
+        password = user_data.get('password')
+        if not password or not isinstance(password, str) or len(password) < 6:
+            raise ValueError("Password must be at least 6 characters")
+
+        
+        is_admin = user_data.get('is_admin', False)
+        is_active = user_data.get('is_active', True)
+
+        if not isinstance(is_admin, bool):
+            raise ValueError("is_admin must be boolean")
+        if not isinstance(is_active, bool):
+            raise ValueError("is_active must be boolean")
+
+        
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            is_admin=is_admin,
+            is_active=is_active,
+        )
+
+        repo.add(user)
+        return user
+    
+    def login(self, email, password, repo):
+        if not email or not isinstance(email, str) or '@' not in email:
+            raise ValueError("Valid email is required")
+
+        if not password or not isinstance(password, str):
+            raise ValueError("Password is required and must be a string")
+
+
+        user = repo.get_by_attribute('email', email)
+        if not user:
+            raise ValueError("Incorrect email or password")
+
+
+        if not user.check_password(password):
+            raise ValueError("Incorrect email or password")
+
+        if user.is_active != True:
+            raise ValueError("User is not active")
+
+        return True
+
+    def get_user_info(self, user_id, repo):
+            
+        if not isinstance(user_id, str):
+            raise ValueError("User ID must be a string")
+
+        user = repo.get(user_id)
+        if not user:
+            raise ValueError("User not found")
+
+
+        return {
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email,
+            "is_admin": user.is_admin,
+            "is_active": user.is_active,
+            "created_at": user.created_at.isoformat(),
+            "updated_at": user.updated_at.isoformat()}
+
+    def update_user(self, user_id: str, user_data: dict, repo):
+
+        if not isinstance(user_id, str):
+            raise ValueError("User ID must be a string")
+
+        user = repo.get(user_id)
+        if not user:
+            raise ValueError("User not found")
+
+        if "first_name" in user_data:
+            if not isinstance(user_data["first_name"], str) or len(user_data["first_name"]) > 50:
+                raise ValueError("Invalid first name")
+            user.first_name = user_data["first_name"]
+
+        if "last_name" in user_data:
+            if not isinstance(user_data["last_name"], str) or len(user_data["last_name"]) > 50:
+                raise ValueError("Invalid last name")
+            user.last_name = user_data["last_name"]
+
+        if "email" in user_data:
+            email = user_data["email"]
+        if not isinstance(email, str):
+            raise ValueError("Invalid email")
+
+        pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        if not re.match(pattern, email):
+            raise ValueError("Invalid email format")
+
+        user.email = email
+
+        if "password" in user_data:
+            if not isinstance(user_data["password"], str) or len(user_data["password"]) < 6:
+                raise ValueError("Password must be at least 6 characters")
+            user.password = (user_data["password"])
+
+        if "is_admin" in user_data:
+            if not isinstance(user_data["is_admin"], bool):
+                raise ValueError("is_admin must be boolean")
+            user.is_admin = user_data["is_admin"]
+
+        if "is_active" in user_data:
+            if not isinstance(user_data["is_active"], bool):
+                raise ValueError("is_active must be boolean")
+            user.is_active = user_data["is_active"]
+
+        return True
+    
+    @staticmethod
+    def delete_user(user_id: str, user_repo, place_repo):
+
+        if not isinstance(user_id, str):
+            raise ValueError("User ID must be a string")
+
+        user = user_repo.get(user_id)
+        if not user:
+            raise ValueError("User not found")
+        
+        places = place_repo.get_all()
+        for place in places:
+            if hasattr(place, "user") and place.user.id == user_id:
+                raise ValueError("Cannot delete user with existing places")
+
+        return user_repo.delete(user_id)
+    
+    def get_all_users(self, repo):
+        users = repo.get_all()
+        return users
