3f77d6a14968d5d277f2a16e3b1a03b434c452f7
Arabian CowBoy - Surface <midoxp@gmail.com>
2026-01-21 17:31:03 +0300

    feat: Initialize Flask app with Flask-RESTX API and register core namespaces for users, places, reviews, and amenities.

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -5,8 +5,9 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as review_ns
 from app.api.v1.amenities import api as amenities_ns
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
 
     api = Api(
         app,
