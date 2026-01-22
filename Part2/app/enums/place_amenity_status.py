d6aac42bb80c2daeb8765f4c1857f79df1a1923d
Nawaf <Alzhraniii999@gmail.com>
2026-01-22 11:49:00 +0300

    Remove Part1 and Part2 from part3 branch

--- Part2/app/enums/place_amenity_status.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class PlaceAmenityStatus(Enum):
-    ACTIVE = "Active"
-    DISABLED = "Disabled"
-    REMOVED = "Removed"
--- Part2/app/enums/place_status.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class PlaceStatus(Enum):
-    AVAILABLE = "available"
-    BOOKED = "booked"
-    UNAVAILABLE = "unavailable"
-    UNDER_MAINTENANCE = "Under Maintenance"
